b88567aa527181127e67110566101cebf1d218e1
Arnold Su <arnoldtingsu@gmail.com>
2018-11-17 04:15:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+dwarves = ["Doc", "Bashful", "Sleepy", "Sneezy", "Happy", "Grumpy", "Dopey"]
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf,index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+ roll_call_dwarves(dwarves)
+ 
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|calls| calls.capitalize + "!"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  
+  
 end
 
 def find_the_cheese# code an argument here
